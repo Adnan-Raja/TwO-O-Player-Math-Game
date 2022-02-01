50893446238d79592d306785fac25566737f99b1
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-02-01 00:45:26 -0500

    updated: fixed bugs

--- Player_One.rb
+++ Player_One.rb
@@ -1,6 +1,6 @@
 class Player_One
   attr_accessor :name, :value, :question
 end
-Player_1 = Player_One.new()
+Player_1 = Player_One.new
 Player_1.name = "Player 1"
 Player_1.value = 3
--- Player_Two.rb
+++ Player_Two.rb
@@ -1,6 +1,6 @@
 class Player_Two
   attr_accessor :name, :question, :value
 end
-Player_2 = Player_One.new()
+Player_2 = Player_One.new
 Player_2.name = "Player 2"
 Player_2.value = 3
